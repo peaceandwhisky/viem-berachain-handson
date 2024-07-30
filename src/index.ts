a534e99ae087df6d1ed504b44414b19f8dfad919
peaceandwhisky <pioneerandf@gmail.com>
2024-07-30 09:44:42 +0900

    add getAddressBalance

--- src/index.ts
+++ src/index.ts
@@ -23,15 +23,29 @@ const berachainTestnet = {
   testnet: true
 };
 
+const client = createPublicClient({
+  chain: berachainTestnet,
+  transport: http(berachainTestnet.rpcUrls.default.http[0]),
+})
+
 async function getBlockNumber() {
-  const client = createPublicClient({
-    chain: berachainTestnet,
-    transport: http(berachainTestnet.rpcUrls.default.http[0]),
-  })
   
   const blockNumber = await client.getBlockNumber()
 
   console.log(blockNumber)
 }
 
+async function getAddressBalance(address: `0x${string}`) {
+  try {
+    const balance = await client.getBalance({ address });
+    console.log(`Balance of ${address}: ${balance} wei`);
+  } catch (error) {
+    console.error(`Failed to fetch balance for address ${address}:`, error);
+  }
+}
+
+
 getBlockNumber().catch(console.error);
+
+const address = "youraddress"; 
+getAddressBalance(address as `0x${string}`).catch(console.error);
