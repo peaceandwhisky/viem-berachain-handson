32f4bb4f981b883ecb14215e25ea6d767dec7b29
peaceandwhisky <pioneerandf@gmail.com>
2024-07-30 09:17:10 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { createPublicClient, http } from 'viem'
+
+const berachainTestnet = {
+  id: 80084, // Berachain Testnet ChainID
+  name: 'Berachain Testnet',
+  network: 'berachain-testnet',
+  nativeCurrency: {
+    name: 'BERA',
+    symbol: 'BERA',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://bartio.rpc.berachain.com/']
+    }
+  },
+  blockExplorers: {
+    default: {
+      name: 'Berachain Explorer',
+      url: 'https://bartio.beratrail.io/'
+    }
+  },
+  testnet: true
+};
+
+async function getBlockNumber() {
+  const client = createPublicClient({
+    chain: berachainTestnet,
+    transport: http(berachainTestnet.rpcUrls.default.http[0]),
+  })
+  
+  const blockNumber = await client.getBlockNumber()
+
+  console.log(blockNumber)
+}
+
+getBlockNumber().catch(console.error);
