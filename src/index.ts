782f1d7419b2c2ffea7f5c01939d10bed9490775
peaceandwhisky <pioneerandf@gmail.com>
2024-08-03 08:00:49 +0900

    fix first state

--- src/index.ts
+++ src/index.ts
@@ -35,17 +35,4 @@ async function getBlockNumber() {
   console.log(blockNumber)
 }
 
-async function getAddressBalance(address: `0x${string}`) {
-  try {
-    const balance = await client.getBalance({ address });
-    console.log(`Balance of ${address}: ${balance} wei`);
-  } catch (error) {
-    console.error(`Failed to fetch balance for address ${address}:`, error);
-  }
-}
-
-
 getBlockNumber().catch(console.error);
-
-const address = "youraddress"; 
-getAddressBalance(address as `0x${string}`).catch(console.error);
